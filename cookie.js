6cc0492527fe8545de67ed1265317df4eb98f7c8
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-11-02 15:44:12 -0600

    functions added

--- cookie.js
+++ cookie.js
@@ -3,11 +3,6 @@ class cursor{
         this.cps = 1;
     }
 }
-class grandma{
-    constructor(){
-        this.cps = 2;
-    }
-}
 class factory{
     constructor(){
         this.cps = 4;
@@ -23,6 +18,16 @@ class shop{
         this.cps = 100;
     }
 }
+function updateCounters(){
+    updateCookieCounter();
+    updateBuildingCounter();
+}
+function updateBuildingCounter() {
+    document.getElementById("cursorDisplay").textContent = numberOfCursors + " Cursors";
+    document.getElementById("factoryDisplay");
+    document.getElementById("mineDisplay");
+    document.getElementById("shopDisplay");
+}
 function updateCookieCounter(){
     COOKIECOUNTER.textContent = Math.floor(cookies) + " cookies";
 }
@@ -32,9 +37,13 @@ function updateCPSbasedOnBuildings(){
         cps = cps + building.cps;
     })
 }
+function assignBuildingCounts(){
+    totalBuildings = buildings.length;
+}
 function animationLoop(){
+    assignBuildingCounts();
     updateCPSbasedOnBuildings();
-    updateCookieCounter();
+    updateCounters();
     requestAnimationFrame(animationLoop)
 }
 
@@ -43,7 +52,9 @@ const COOKIECOUNTER = document.getElementById("cookieDisplay");
 let cookiesPerClick = 1;
 let cps = 0;
 let cookies = 0;
-let buildings = [new cursor(), new shop(), new mine()]
+let buildings = [new cursor(), new shop(), new mine()];
+let totalBuildings = 0;
+let numberOfCursors = 0;
 
 animationLoop();
 COOKIE.addEventListener("click", () => {
