9be4be9645651cbea7384bb25f6e3fbe458638f0
PratyushGuptaIsMe <pratyushngupta@gmail.com>
2025-11-02 16:13:25 -0600

    Added styling, more functionality

--- cookie.js
+++ cookie.js
@@ -19,17 +19,20 @@ class shop{
     }
 }
 function updateCounters(){
-    updateCookieCounter();
+    updateCookieCounters();
     updateBuildingCounter();
 }
 function updateBuildingCounter() {
     document.getElementById("cursorDisplay").textContent = numberOfCursors + " Cursors";
-    document.getElementById("factoryDisplay");
-    document.getElementById("mineDisplay");
-    document.getElementById("shopDisplay");
+    document.getElementById("factoryDisplay").textContent = numberOfFactories + " Factories";
+    document.getElementById("mineDisplay").textContent = numberOfMines + " Mines";
+    document.getElementById("shopDisplay").textContent = numberOfShops + " Shops";
 }
-function updateCookieCounter(){
+function updateCookieCounters(){
     COOKIECOUNTER.textContent = Math.floor(cookies) + " cookies";
+    document.getElementById("cpsDisplay").textContent = cps + " cookies per second";
+    document.getElementById("cpcDisplay").textContent = cookiesPerClick + " cookies per click";
+    document.getElementById("buildingCountDisplay").textContent = buildingCount + " buildings total";
 }
 function updateCPSbasedOnBuildings(){
     cps = 0;
@@ -38,7 +41,7 @@ function updateCPSbasedOnBuildings(){
     })
 }
 function assignBuildingCounts(){
-    totalBuildings = buildings.length;
+    buildingCount = buildings.length;
 }
 function animationLoop(){
     assignBuildingCounts();
@@ -52,14 +55,62 @@ const COOKIECOUNTER = document.getElementById("cookieDisplay");
 let cookiesPerClick = 1;
 let cps = 0;
 let cookies = 0;
-let buildings = [new cursor(), new shop(), new mine()];
+let buildings = [];
 let totalBuildings = 0;
 let numberOfCursors = 0;
+let numberOfFactories = 0;
+let numberOfMines = 0;
+let numberOfShops = 0;
+let buildingCount = numberOfCursors + numberOfFactories + numberOfMines + numberOfShops;
+let GNRLINFO = {
+    buildingPrices: {
+        cursor: 15,
+        factory: 100,
+        mine: 1000,
+        shop: 10000
+    } 
+}
 
-animationLoop();
 COOKIE.addEventListener("click", () => {
     cookies = cookies + cookiesPerClick;
 })
 setInterval(() => {
     cookies = cookies + cps/100;
 }, 10);
+document.getElementById("cursorDisplay").addEventListener("click", () => {
+    if(cookies >= GNRLINFO.buildingPrices.cursor){
+        buildings.push(new cursor());
+        cookies = cookies - GNRLINFO.buildingPrices.cursor;
+        numberOfCursors++;
+    }else{
+        alert("Not enough money")
+    }
+});
+document.getElementById("factoryDisplay").addEventListener("click", () => {
+    if(cookies >= GNRLINFO.buildingPrices.factory){
+        buildings.push(new factory());
+        cookies = cookies - GNRLINFO.buildingPrices.factory;
+        numberOfFactories++;
+    }else{
+        alert("Not enough money")
+    }
+});
+document.getElementById("mineDisplay").addEventListener("click", () => {
+    if(cookies >= GNRLINFO.buildingPrices.mine){
+        buildings.push(new mine());
+        cookies = cookies - GNRLINFO.buildingPrices.mine;
+        numberOfMines++;
+    }else{
+        alert("Not enough money")
+    }
+});
+document.getElementById("shopDisplay").addEventListener("click", () => {
+    if(cookies >= GNRLINFO.buildingPrices.shop){
+        buildings.push(new shop());
+        cookies = cookies - GNRLINFO.buildingPrices.shop;
+        numberOfShops++;
+    }else{
+        alert("Not enough money")
+    }
+});
+animationLoop();
